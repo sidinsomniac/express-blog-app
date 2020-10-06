8a8088456aa0f4f601ef8a811c31670ab82bd159
sidinsomniac <chatterjeesid1993@gmail.com>
2020-10-06 21:40:06 +0530

    Initialize blog project

--- modules/date.js
+++
@@ -1,5 +0,0 @@
-exports.getDate = function () {
-    const today = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    return today.toLocaleDateString('en-US', options);;
-}
